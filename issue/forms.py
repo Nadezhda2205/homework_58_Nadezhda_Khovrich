2dff27741325b66c5d9581e5b2e757ac32b4d91b
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-09 14:24:00 +0600

    Созданы валидаторы max_length_validator, min_length_validator для заголовка

--- issue/forms.py
+++ issue/forms.py
@@ -1,8 +1,29 @@
 from issue.models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField
+from django.core.exceptions import ValidationError
+
+
+def max_length_validator(string):
+    if len(string) > 20:
+        raise ValidationError('Максимальная длина строки 20 символов')
+    return string
+
+def min_length_validator(string):
+    if len(string) < 5:
+        raise ValidationError('Минимальная длина строки 5 символов')
+    return string
 
 
 class TaskForm(ModelForm):
+    summary = CharField(label='Заголовок', validators=(max_length_validator, min_length_validator))
+    
     class Meta:
         model = Task
         fields = ['summary', 'description',  'status',  'type']
+
+    def clean_summary(self):
+        summary = self.cleaned_data.get('summary')
+        if Task.objects.filter(summary=summary).exists():
+            raise ValidationError('Задача с таким названием существует')
+        return summary
+
