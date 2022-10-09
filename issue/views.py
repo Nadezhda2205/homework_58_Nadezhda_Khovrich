a097bf81c3e325373cb04a1058050d033ef22a69
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-09 21:47:42 +0600

    Добавлены бонусные запросы ORM

--- issue/views.py
+++ issue/views.py
@@ -1,4 +1,3 @@
-from multiprocessing import context
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
 from typing import Any
--- task_2.py
+++ task_2.py
@@ -1,4 +1,4 @@
-from issue.models import Task
+from issue.models import Task, Type
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from django.db.models import Q
@@ -19,4 +19,28 @@ Task.objects.filter(
 
 #Задачи, в названии которых содержится слово "bug" в любом регистре или относящиеся к типу "Баг", 
 # имеющие НЕ закрытый статус.
-task_2 = Task.objects.filter(Q(summary__iregex='bug') | Q(type__name='Bug')).exclude(status__name='Done')
+Task.objects.filter(Q(summary__iregex='bug') | Q(type__name='Bug')).exclude(status__name='Done')
+
+
+# Для всех задач только следующие поля: id, название задачи, название типа и название статуса.
+Task.objects.all().values('id', 'summary', 'status__name', 'type__name')
+
+
+#Задачи, где краткое описание совпадает с полным.
+summaries = Task.objects.all().values('summary')
+summaries_list = []
+for item in summaries:
+    summaries_list.append(item.get('summary'))
+
+Task.objects.filter(description__in=summaries_list)
+
+
+#Количество задач по каждому типу.
+types = Type.objects.all().values('name')
+types_list = []
+for item in types:
+    types_list.append(item.get('name'))
+counts = {}
+for item in types_list:
+    summaries = Task.objects.filter(type__name=item).count()
+    counts[item] = summaries
